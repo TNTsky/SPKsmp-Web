b05bfb5d0821cf61c96ec5ebc9f537e2a2ba6006
TNTsky <TNTsky.com@gmail.com>
2024-07-08 07:11:45 +0800

    chore: Update community page UI

--- community_tooltip.js
+++ community_tooltip.js
@@ -4,7 +4,7 @@ jq.src = './jquery-ui-1.13.3.custom/external/jquery/jquery.js';
 document.querySelector('head').appendChild(jq);
 
 function load_jquery() {
-
+    // jQuery loaded
 };
 
 let jqui = document.createElement('script');
@@ -13,16 +13,20 @@ jqui.src = './jquery-ui-1.13.3.custom/jquery-ui.js';
 document.querySelector('head').appendChild(jqui);
 
 function load_jqueryui() {
-    $( () => {
-        $( "a" ).tooltip({
+    $(() => {
+        $(".social-link").tooltip({
+            content: function() {
+                // return $(this).prop('title') + "<br><br>" + $(this).find('.social-description').text();
+                return $(this).prop('title');
+            },
             show: null,
             position: {
-              my: "left top",
-              at: "left bottom"
+                my: "center bottom",
+                at: "center top-10"
             },
-            open: ( event, ui ) => {
-              ui.tooltip.animate({ top: ui.tooltip.position().top + 10 }, "fast" );
+            open: (event, ui) => {
+                ui.tooltip.animate({ top: ui.tooltip.position().top + 10 }, "fast");
             }
-          });
+        });
     });
 };
